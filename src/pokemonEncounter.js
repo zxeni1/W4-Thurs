6699ad9299809b1fe888ebc95d0ea0bea75d162e
zxeni1 <zacxfitness@gmail.com>
2024-11-07 19:51:28 +1000

    feat. add photo, name, types & sound

--- src/pokemonEncounter.js
+++ src/pokemonEncounter.js
@@ -2,15 +2,59 @@ console.log("Pokemon Journey begins...");
 
 const encounterButton = document.getElementById("pokemonEncounterButton");
 const pokemonRenderArea = document.getElementById("encounteredPokemonArea");
+const pokemonContainerDiv = document.getElementById("pokemonContainer");
 
 function renderPokemonData(pokemonData){
-    pokemonRenderArea.innerText = pokemonData.name;
+    if (!pokemonData.name){
+        return;
+    }
+    // This div has class now
+    pokemonContainerDiv.classList += "pokemonCardEntry";
+
+    let pokemonHeading = document.createElement("h1");
+    pokemonHeading.innerText = pokemonData.name;
+    pokemonContainerDiv.appendChild(pokemonHeading);
+
+    let pokemonImage = document.createElement("img");
+    pokemonImage.src = pokemonData.image;
+    pokemonContainerDiv.appendChild(pokemonImage);
+    
+    let pokemonTypesHeading = document.createElement("h3");
+    pokemonTypesHeading.innerText = "Types:";
+    pokemonContainerDiv.appendChild(pokemonTypesHeading);
+
+    let pokemonTypeList = document.createElement("ul");
+    // Loop through the array of pokemonData
+    pokemonData.types.forEach((typeObject) => {
+        // Create li element for each type
+        let pokemonTypeListItem = document.createElement("li");
+        // Add name to li
+        pokemonTypeListItem.innerText = typeObject.type.name;
+        // Append it to the ul
+        pokemonTypeList.appendChild(pokemonTypeListItem);
+    });
+    pokemonContainerDiv.appendChild(pokemonTypeList);
+
+    let pokemonAudioButton = document.createElement("button");
+    pokemonAudioButton.innerText = "Play Sound";
+    pokemonAudioButton.addEventListener("click", ()=>{
+        let pokemonAudioObject = new Audio(pokemonData.sound);
+        pokemonAudioObject.play();
+    });
+    pokemonContainerDiv.appendChild(pokemonAudioButton);
+
+    pokemonRenderArea.appendChild(pokemonContainerDiv);
+}
+
+function getRandomPokemonId(){
+    // Random number between 1 and 1025 (max number of Pokemon)
+    return Math.ceil(Math.random() * 1025);
 }
 
 async function getPokemon(){
     console.log("Looking for a wild Pokemon");
     
-    let apiResponse = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+    let apiResponse = await fetch("https://pokeapi.co/api/v2/pokemon/" + getRandomPokemonId());
     let apiData = await apiResponse.json();
 
     // Fetch name, type, image, cry
